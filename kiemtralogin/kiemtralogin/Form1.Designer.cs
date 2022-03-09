ac8207d44710d9e7b71fb969fb43317ceaa92de7
DESKTOP-KVV5GLK\HUY <dqhuy711@gmail.com>
2022-03-09 14:34:12 +0700

    Thay đổi pass, thêm bunton

--- kiemtralogin/kiemtralogin/Form1.Designer.cs
+++ kiemtralogin/kiemtralogin/Form1.Designer.cs
@@ -32,7 +32,8 @@ namespace kiemtralogin
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -56,23 +57,33 @@ namespace kiemtralogin
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(73, 102);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "MatKhau";
+            this.label2.Text = "PassWord";
             // 
-            // txtmatkhau
+            // txtpassword
             // 
-            this.txtmatkhau.Location = new System.Drawing.Point(154, 99);
-            this.txtmatkhau.Name = "txtmatkhau";
-            this.txtmatkhau.Size = new System.Drawing.Size(100, 20);
-            this.txtmatkhau.TabIndex = 1;
+            this.txtpassword.Location = new System.Drawing.Point(154, 99);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(164, 147);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 2;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -88,7 +99,8 @@ namespace kiemtralogin
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button login;
     }
 }
 
